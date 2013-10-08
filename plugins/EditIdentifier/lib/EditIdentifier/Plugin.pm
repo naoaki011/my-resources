5655da368b088fada2b078c26a454b1ecdfce7d0
naoaki011 <naoaki@orion.ocn.ne.jp>
2013-10-08 17:16:21 +0900

    fix

--- plugins/EditIdentifier/lib/EditIdentifier/Plugin.pm
+++ plugins/EditIdentifier/lib/EditIdentifier/Plugin.pm
@@ -1,19 +1,13 @@
 package EditIdentifier::Plugin;
-use MT::Template; 
+use MT::Template;
 use strict;
 
 sub _edit_template_param {
     my ( $cb, $app, $param, $tmpl ) = @_;
     my $plugin = MT->component( 'EditIdentifier' );
-    my $pointer_field = ( $param->{type} eq 'index' ) ? $tmpl->getElementById( 'outfile' )
+    my $pointer_field = ( $param->{type} eq 'index' ) ? $tmpl->getElementById( 'identifier' )
                                                       : $tmpl->getElementById( 'linked_file' );
     if (( $pointer_field )&&( MT->config->DebugMode > 0 )) {
-        my $nodeset = $tmpl->createElement( 'app:setting', { id => 'identifier',
-                                                             label => $plugin->translate( 'Identifier' ),
-                                                             label_class => 'top-level',
-                                                             required => 0,
-                                                           }
-                                          );
         my $innerHTML = <<'MTML';
 <__trans_section component="EditIdentifier">
 <label>
@@ -21,12 +15,20 @@ sub _edit_template_param {
 </label>
 </__trans_section>
 MTML
-        $nodeset->innerHTML( $innerHTML );
         if ( $param->{type} eq 'index' ) {
-            $tmpl->insertAfter( $nodeset, $pointer_field );
+            $pointer_field->setAttribute( 'label', $plugin->translate( 'Identifier' ) );
+            $pointer_field->innerHTML( $innerHTML );
         } else {
+            my $nodeset = $tmpl->createElement( 'app:setting', { id => 'identifier',
+                                                                 label => $plugin->translate( 'Identifier' ),
+                                                                 label_class => 'top-level',
+                                                                 required => 0,
+                                                               }
+                                              );
+            $nodeset->innerHTML( $innerHTML );
             $tmpl->insertBefore( $nodeset, $pointer_field );
-        }     }
+        }
+    }
 }
 
 1;
